dc4960c4d3df94ac8d5b5b526e35915533ece589
ltagliamonte <ltagliamonte@users.noreply.github.com>
2023-06-17 10:32:04 -0700

    rename

--- main.go
+++ main.go
@@ -4,16 +4,16 @@
 package main
 
 import (
+	"terraform-provider-authz/authz"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-
-	"terraform-provider-hashicups/hashicups"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
-			return hashicups.Provider()
+			return authz.Provider()
 		},
 	})
 }
