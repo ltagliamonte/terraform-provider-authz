5f299e5668efee8611be16eea7b0320f981604a8
ltagliamonte <ltagliamonte@users.noreply.github.com>
2023-06-24 13:53:10 -0700

    remove unused func

--- authz/provider_test.go
+++ authz/provider_test.go
@@ -4,7 +4,6 @@
 package authz
 
 import (
-	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -28,13 +27,4 @@ func TestProvider(t *testing.T) {
 
 func TestProvider_impl(t *testing.T) {
 	var _ *schema.Provider = Provider()
-}
-
-func testAccPreCheck(t *testing.T) {
-	if err := os.Getenv("AUTHZ_HOST"); err == "" {
-		t.Fatal("AUTHZ_HOST must be set for acceptance tests")
-	}
-	if err := os.Getenv("AUTHZ_CLIENT_SECRET"); err == "" {
-		t.Fatal("AUTHZ_CLIENT_SECRET must be set for acceptance tests")
-	}
 }
